5f9b4fd822f2c4af776a9847ff9dff2b532f651d
dsorthep <detvisiths@yahoo.com>
2019-11-21 18:25:06 -0600

    current date displays under heading

--- scripts.js
+++ scripts.js
@@ -1,2 +1,10 @@
-let currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+let currentTime = moment().format('MMMM Do YYYY');
 console.log(currentTime);
+
+function displayDate () {
+    document.getElementById("current-day").innerHTML = currentTime;
+};
+displayDate();
+
+
